7e1d1f614bd6c12c7c027dde1978c9faa0e2e68f
kazenone <kazenone@gmail.com>
2026-09-13 12:30:07 +0100

    Crosspost events into CRPC

--- common.js
+++ common.js
@@ -18,7 +18,36 @@ function sendHook(hookId, hookToken, messageData) {
   return hook.send(messageData).catch(err => console.error(err));
 }
 
+function nitroSplit(message, size) {
+  if (!size) {
+    size = 2000
+  }
+  
+  if (message.length <= size) {
+    return [{ content: message }]
+  }
+  
+  var paragraphs = message.split('\n')
+  var segments = [];
+  var partial = paragraphs.splice(0,1)[0];
+  for (const next of paragraphs) {
+    var partialNext = `${partial}\n${next}`
+    // Still under the non-nitro character length
+    if (partialNext.length < size) {
+      partial = partialNext
+    } else {
+      segments.push({ content: partial });
+      partial = next
+    }
+  }
+  
+  segments.push({ content: partial })
+
+  return segments  
+}
+
 module.exports = {
   styledEmbed: styledEmbed,
-  sendHook: sendHook
+  sendHook: sendHook,
+  nitroSplit: nitroSplit
 };
---
+++ events/publishEvents.js
@@ -0,0 +1,29 @@
+const { Events } = require('discord.js');
+const auth = require('../auth.json');
+const common = require('../common.js');
+
+module.exports = {
+  name: Events.ThreadCreate,
+  execute: async (client, args) => {
+    let channel = args[0];
+    
+    console.log(`Created new thread: ${channel.id}`)
+    
+    // Only look at #events-board threads
+    if (channel.parentId != '1172943658287386774') {
+      return;
+    }
+    
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    
+    var firstPost = await channel.fetchStarterMessage();
+    
+    // Need to split nitro-length messages to post as a bot
+    var messageSegments = common.nitroSplit(`Posted from <#${channel.id}>\n${firstPost.content}`);
+    messageSegments[messageSegments.length-1].attachments = firstPost.attachments;
+    
+    for (const msg of messageSegments) {
+      common.sendHook(auth.eventsWebhook.id, auth.eventsWebhook.token, msg);
+    }
+  }
+}
